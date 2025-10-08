96d9fbb4273f115625e5250d46cbf2a61856916c
Gurudas Bhardwaj <gursad5@gmail.com>
2025-10-08 09:42:36 +0530

    Update

--- redis/redisClient.js
+++ redis/redisClient.js
@@ -1,11 +1,7 @@
 // src/redis/redisClient.js
 import Redis from "ioredis";
 
-const redis = new Redis({
-  host: "127.0.0.1",
-  port: 6379,
-  // password: process.env.REDIS_PASSWORD, // if required
-});
+const redis = new Redis(process.env.REDIS_URL); // Use Render env variable
 
 redis.on("connect", () => console.log("✅ Redis connected"));
 redis.on("error", (err) => console.error("Redis error:", err));
